60fcc76643cb42a7106c2c8b8531749b19de4450
Logan <lgwarlick@gmail.com>
2022-01-25 13:28:13 -0600

    added base level classes

---
+++ src/main/java/lgwarlick/cards/model/CardReleaseSet.java
@@ -0,0 +1,16 @@
+package lgwarlick.cards.model;
+
+public class CardReleaseSet extends BaseEntity {
+
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
