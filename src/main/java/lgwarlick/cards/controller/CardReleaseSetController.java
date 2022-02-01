2d4788058078fc026f41e8ef0c8e149121af929c
Logan <lgwarlick@gmail.com>
2022-01-31 20:18:06 -0600

    CardReleaseSetController.java and cardreleaseset.html now return a list of stored objects

--- src/main/java/lgwarlick/cards/controller/CardReleaseSetController.java
+++ src/main/java/lgwarlick/cards/controller/CardReleaseSetController.java
@@ -1,14 +1,25 @@
 package lgwarlick.cards.controller;
 
+import lgwarlick.cards.service.CardReleaseSetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CardReleaseSetController {
 
+    private final CardReleaseSetService cardReleaseSetService;
+
+    public CardReleaseSetController(CardReleaseSetService cardReleaseSetService) {
+        this.cardReleaseSetService = cardReleaseSetService;
+    }
+
     @RequestMapping("/set")
-    public String allCardsInReleaseSet(){
-        return "cardreleaseset";
+    public String allReleaseSets(Model model){
+
+        model.addAttribute("cardreleaseset", cardReleaseSetService.findAll());
+
+        return "cardreleaseset.html";
     }
 
 }
