378a4161e6906ba8c4b4dd032449625d38ee23bf
Logan Warlick <36140675+lgwarlick@users.noreply.github.com>
2022-02-18 10:55:03 -0600

    Delete schema.sql

--- src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE CARDRELEASESET(
-                               ID LONG NOT NULL AUTO_INCREMENT,
-                               NAME VARCHAR(256) NOT NULL,
-                               PRIMARY KEY (ID)
-);
