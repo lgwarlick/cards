04ad7a154dc3f5d051fba1b9ab42a4811695c400
Logan <lgwarlick@gmail.com>
2022-02-28 16:01:32 -0600

    removed data.sql

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO card_release_set (name, release_date) VALUES ('What Lies Within', 'December 2012');
-INSERT INTO card_release_set (name, release_date) VALUES ('Trace Amount', 'January 2013');
-INSERT INTO card_release_set (name, release_date) VALUES ('Cyber Exodus', 'February 2013');
-INSERT INTO card_release_set (name, release_date) VALUES ('A Study in Static', 'March 2013');
-INSERT INTO card_release_set (name, release_date) VALUES ('Humanity''s Shadow', 'May 2013');
-INSERT INTO card_release_set (name, release_date) VALUES ('Future Proof', 'June 2013');
-
-INSERT INTO card (name, faction, card_type) VALUES ('Astroscript Pilot Program', 'NBN', 'Agenda');
