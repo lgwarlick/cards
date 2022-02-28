679a1fb316f4e09bb3d688d4b9e22654a46e72df
Logan <lgwarlick@gmail.com>
2022-02-28 16:04:07 -0600

    Multiple revisions, loads 10 cards and 2 sets on start-up

--- src/main/java/lgwarlick/cards/bootstrap/DataLoader.java
+++ src/main/java/lgwarlick/cards/bootstrap/DataLoader.java
@@ -4,69 +4,151 @@ import lgwarlick.cards.model.Card;
 import lgwarlick.cards.model.CardReleaseSet;
 import lgwarlick.cards.model.CardType;
 import lgwarlick.cards.model.Faction;
-import lgwarlick.cards.service.CardReleaseSetService;
-import lgwarlick.cards.service.CardService;
-import org.springframework.boot.CommandLineRunner;
+import lgwarlick.cards.repository.CardReleaseSetRepository;
+import lgwarlick.cards.repository.CardRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
-public class DataLoader implements CommandLineRunner {
+public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-private final CardReleaseSetService cardReleaseSetService;
-private final CardService cardService;
+    private final CardRepository cardRepository;
+    private final CardReleaseSetRepository cardReleaseSetRepository;
 
-    public DataLoader(CardReleaseSetService cardReleaseSetService, CardService cardService) {
-        this.cardReleaseSetService = cardReleaseSetService;
-        this.cardService = cardService;
+    public DataLoader(CardRepository cardRepository, CardReleaseSetRepository cardReleaseSetRepository) {
+        this.cardRepository = cardRepository;
+        this.cardReleaseSetRepository = cardReleaseSetRepository;
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void onApplicationEvent(ContextRefreshedEvent event) {
 
-        CardReleaseSet release1 = new CardReleaseSet();
-        release1.setName("What Lies Head");
-        release1.setReleaseDate("December 2012");
-        cardReleaseSetService.save(release1);
+        populateCards();
 
-        CardReleaseSet release2 = new CardReleaseSet();
-        release2.setName("Trace Amounts");
-        release2.setReleaseDate("January 2013");
-        cardReleaseSetService.save(release2);
+        /**
+         * CARDS
+         */
 
-        CardReleaseSet release3 = new CardReleaseSet();
-        release3.setName("Cyber Exodus");
-        release3.setReleaseDate("February 2013");
-        cardReleaseSetService.save(release3);
+        Card card5 = new Card();
+        card5.setName("Gordian Blade");
+        card5.setCardType(CardType.PROGRAM);
+        card5.setFaction(Faction.SHAPER);
+        card5.setInstallCost(4);
+        card5.setMemoryCost(1);
+        card5.setStrength(2);
+        card5.setCardText("1: Break code gate subroutine. 1: +1 strength for remainder of this run");
 
+        cardRepository.save(card5);
+
+        Card card6 = new Card();
+        card6.setName("Hedge Fund");
+        card6.setFaction(Faction.NEUTRAL);
+        card6.setCardType(CardType.OPERATION);
+        card6.setPlayCost(5);
+        card6.setCardText("Gain 9");
+
+        cardRepository.save(card6);
+
+        Card card7 = new Card();
+        card7.setName("Special Order");
+        card7.setCardType(CardType.EVENT);
+        card7.setFaction(Faction.CRIMINAL);
+        card7.setPlayCost(1);
+        card7.setCardText("Search your stack for an icebreaker, reveal is, and add it to your grip. Shuffle your stack.");
+
+        cardRepository.save(card7);
+
+        Card card8 = new Card();
+        card8.setName("Public Sympathy");
+        card8.setCardType(CardType.RESOURCE);
+        card8.setFaction(Faction.NEUTRAL);
+        card8.setPlayCost(2);
+        card8.setCardText("Your maximum hand size is increased by 2");
+
+        cardRepository.save(card8);
+
+        Card card9 = new Card();
+        card9.setName("Weyland Consortium");
+        card9.setCardType(CardType.CORPORATIONIDENTITY);
+        card9.setFaction(Faction.WEYLAND_CONSORTIUM);
+        card9.setMinimumDeckSize(45);
+        card9.setInfluenceLimit(15);
+        card9.setCardText("Gain 1 whenever you play a transaction operation");
+
+        cardRepository.save(card9);
+
+        Card card10 = new Card();
+        card10.setName("Wall of Static");
+        card10.setCardType(CardType.ICE);
+        card10.setFaction(Faction.NEUTRAL);
+        card10.setRezCost(3);
+        card10.setStrength(3);
+        card10.setCardText("End the run");
+
+        cardRepository.save(card10);
+
+        /**
+         * SETS
+         */
+
+        CardReleaseSet set1 = new CardReleaseSet();
+        set1.setName("What Lies Ahead");
+        set1.setReleaseDate("December 2012");
+
+
+        cardReleaseSetRepository.save(set1);
+
+        CardReleaseSet set2 = new CardReleaseSet();
+        set2.setName("Trace Amounts");
+        set2.setReleaseDate("January 2013");
+
+        cardReleaseSetRepository.save(set2);
+
+    }
+
+    private void populateCards(){
         Card card1 = new Card();
-        card1.setName("Acme Consulting");
-        card1.setCardType(CardType.CORPORATIONIDENTITY);
+        card1.setName("Breaking News");
         card1.setFaction(Faction.NBN);
-        card1.setCardText("The Runner is considered to have 1 additional tag (even if they have 0) during encounters with the outermost piece of ice protecting any server.");
-        cardService.save(card1);
+        card1.setCardType(CardType.AGENDA);
+        card1.setAdvancementRequirement(2);
+        card1.setAgendaPoints(1);
+        card1.setCardText("When you score Breaking News, give the Runner 2 tags.\n" +
+                "\n" +
+                "When the turn on which you scored Breaking News ends, the Runner loses 2 tags.");
+
+        cardRepository.save(card1);
 
         Card card2 = new Card();
-        card2.setName("Armitage Codebusting");
-        card2.setCardType(CardType.RESOURCE);
+        card2.setName("Hedge Fund");
+        card2.setCardType(CardType.OPERATION);
         card2.setFaction(Faction.NEUTRAL);
-        card2.setCardText("Place 12 from the bank on Armitage Codebusting");
-        card2.setInstallCost(1);
-        cardService.save(card2);
+        card2.setPlayCost(5);
+        card2.setCardText("Gain 9 credit.");
+
+        cardRepository.save(card2);
 
         Card card3 = new Card();
-        card3.setName("Bellona");
+        card3.setName("Government Contracts");
+        card3.setFaction(Faction.WEYLAND_CONSORTIUM);
         card3.setCardType(CardType.AGENDA);
-        card3.setFaction(Faction.NBN);
-        card3.setCardText("As an additional cost to steal this agenda, the Runner must pay 5credit. When you score this agenda, gain 5credit.");
-        card3.setAdvancementRequirement(10);
-        cardService.save(card3);
+        card3.setAgendaPoints(3);
+        card3.setAdvancementRequirement(5);
+        card3.setCardText("Spend 2: Gain 4");
+
+        cardRepository.save(card3);
 
         Card card4 = new Card();
-        card4.setName("Degree Mill");
-        card4.setCardType(CardType.AGENDA);
-        card4.setFaction(Faction.NBN);
-        card4.setCardText("As an additional cost to steal Degree Mill, the Runner must shuffle 2 installed Runner cards into the stack.");
-        card4.setAdvancementRequirement(5);
-        cardService.save(card4);
+        card4.setName("Diesel");
+        card4.setCardType(CardType.EVENT);
+        card4.setFaction(Faction.SHAPER);
+        card4.setPlayCost(0);
+        card4.setCardText("Draw 3 cards");
+
+        cardRepository.save(card4);
     }
 }
